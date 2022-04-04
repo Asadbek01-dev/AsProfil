a531da9b4af86cbc24b3eafe0eea96fa1216625b
Asadbek01-dev <turgunovasadillo61@gmail.com>
2022-04-04 13:53:49 +0500

    vide qoydim

--- css/js/index.js
+++ css/js/index.js
@@ -19,3 +19,14 @@ function myMap() {
       messa.value = null;
     }
 
+    $(document).ready(function(){
+      $("video").prop('muted', true);
+    
+      $("video").click( function (){
+          $(this).prop('muted', !$(this).prop('muted'));
+      });
+    
+      $(".play-video").click( function (){
+          $("video").prop('muted', !$("video").prop('muted'));
+      });
+    });
